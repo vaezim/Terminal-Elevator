aa92d4a662bace0765ffb495fbede06be714f522
Max Remzi <91099081+vaezim@users.noreply.github.com>
2025-06-11 19:51:17 -0400

    Version 1.0

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <thread>
 
+#include "elevator_sim_lib.hpp"
+
 
 int main()
 {
-    
+    constexpr int numFloors{ 30 };
+    constexpr int numElevators{ 8 };
+    constexpr float passengersPerMinute{ 120.0 };
+
+    ElevatorSimLib::Scheduler scheduler(numFloors, numElevators, passengersPerMinute);
+
+    std::thread displayThread(&ElevatorSimLib::Scheduler::Display, &scheduler);
+    scheduler.Run();
+    displayThread.join();
 }
